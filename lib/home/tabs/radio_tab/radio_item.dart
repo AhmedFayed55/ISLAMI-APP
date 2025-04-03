6ed283bf3cdcd2ba8b539eb5cbc9c1b403cc4672
Fayed <anaa71137@gmail.com>
2025-04-03 19:51:26 +0200

    Radio new ui with api. Redo sebha tab with 2 styles.

---
+++ lib/home/tabs/radio_tab/radio_item.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app_new/home/app_colors.dart';
+import 'package:islami_app_new/provider/radio_manager_provider.dart';
+import 'package:provider/provider.dart';
+
+class RadioItem extends StatefulWidget {
+  String name;
+  String url;
+
+  RadioItem({required this.name, required this.url});
+
+  @override
+  State<RadioItem> createState() => _RadioItemState();
+}
+
+class _RadioItemState extends State<RadioItem> {
+  bool isVolumeUp = true;
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Consumer<RadioManagerProvider>(
+      builder: (context, RadioManagerProvider provider, child) {
+        return Container(
+          height: height * .15,
+          // padding: EdgeInsets.only(top: height * .02),
+          margin: EdgeInsets.only(bottom: height * .015),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              image: AssetImage("assets/images/radio_back_nv1.png"),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                widget.name,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: AppColor.black,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      provider.play(widget.url);
+                    },
+                    icon: Icon(
+                      (provider.isPlaying &&
+                              provider.currentPlayingUrl == widget.url)
+                          ? Icons.pause
+                          : Icons.play_circle,
+                      color: AppColor.black,
+                      size: 35,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (provider.currentPlayingUrl == widget.url) {
+                        provider.stop();
+                      }
+                    },
+                    icon: Icon(
+                      Icons.stop_circle_outlined,
+                      color: AppColor.black,
+                      size: 35,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      isVolumeUp = !isVolumeUp;
+                      provider.setVolume(isVolumeUp ? 2.0 : 0.0);
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      isVolumeUp ? Icons.volume_up_rounded : Icons.volume_off,
+                      color: AppColor.black,
+                      size: 35,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/home/tabs/radio_tab/radio_tab.dart
+++ lib/home/tabs/radio_tab/radio_tab.dart
@@ -1,11 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app_new/api/api_manager.dart';
+import 'package:islami_app_new/home/app_colors.dart';
+import 'package:islami_app_new/home/tabs/radio_tab/radio_item.dart';
+import 'package:islami_app_new/models/radio_response_model.dart';
+import 'package:islami_app_new/models/reciters_response_model.dart';
 
-class RadioTab extends StatelessWidget {
+class RadioTab extends StatefulWidget {
   const RadioTab({super.key});
 
+  @override
+  State<RadioTab> createState() => _RadioTabState();
+}
+
+class _RadioTabState extends State<RadioTab> {
 //
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var height = MediaQuery
+        .of(context)
+        .size
+        .height;
+    var width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: width * .035),
+      child: DefaultTabController(
+          length: 2,
+          child: Column(
+            spacing: height * .02,
+            children: [
+              Image.asset("assets/images/bar.png"),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: const Color(0xb3202020)
+                ),
+                child: TabBar(
+                    dividerHeight: 0,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    indicator: BoxDecoration(
+                        color: AppColor.gold,
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    labelStyle: TextStyle(fontSize: 16, color: AppColor.black),
+                    unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: AppColor.white),
+                    tabs: const [
+                      Tab(text: 'Radio',),
+                      Tab(text: 'Reciters',),
+                    ]),
+              ),
+              Expanded(
+                child: TabBarView(children: [
+                  FutureBuilder<RadioResponseModel>(
+                    future: ApiManager.getRadioData(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(color: AppColor
+                              .gold,),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Column(
+                          children: [
+                            const Text('SomeThing went wrong',
+                              style: TextStyle(color: AppColor.gold),),
+                            ElevatedButton(onPressed: () {
+                              ApiManager.getRadioData();
+                              setState(() {});
+                            },
+                                child: const Text('Try Again')),
+                          ],
+                        );
+                      }
+                      RadioResponseModel data = snapshot.data!;
+                      return ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: data.radios!.length,
+                        itemBuilder: (context, index) {
+                          return RadioItem(
+                            name: data.radios![index].name ?? "",
+                            url: data.radios![index].url ?? "",);
+                        },
+                      );
+                    },),
+                  FutureBuilder<RecitersResponseModel>(
+                    future: ApiManager.getRecitersData(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(color: AppColor
+                              .gold,),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Column(
+                          children: [
+                            const Text('SomeThing went wrong',
+                              style: TextStyle(color: AppColor.gold),),
+                            ElevatedButton(onPressed: () {
+                              ApiManager.getRecitersData();
+                              setState(() {});
+                            },
+                                child: const Text('Try Again')),
+                          ],
+                        );
+                      }
+                      RecitersResponseModel data = snapshot.data!;
+                      return FutureBuilder<RecitersResponseModel>(
+                        future: ApiManager.getRecitersData(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(
+                                color: AppColor.gold,),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Column(
+                              children: [
+                                const Text('SomeThing went wrong',
+                                  style: TextStyle(color: AppColor.gold),),
+                                ElevatedButton(onPressed: () {
+                                  ApiManager.getRecitersData();
+                                  setState(() {});
+                                },
+                                    child: const Text('Try Again')),
+                              ],
+                            );
+                          }
+                          RecitersResponseModel data = snapshot.data!;
+                          return ListView.builder(
+                            padding: EdgeInsets.zero,
+                            itemCount: data.reciters!.length,
+                            itemBuilder: (context, index) {
+                              return RadioItem(
+                                  name: data.reciters![index].name ?? "",
+                                  url: "${data.reciters![index].moshaf![0]
+                                      .server}112.mp3");
+                            },
+                          );
+                        },);
+                    },),
+
+                ]),
+              )
+            ],
+          )
+      ),
+    );
   }
 }
